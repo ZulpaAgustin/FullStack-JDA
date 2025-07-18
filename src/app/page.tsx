0ed470fa7f090a67cf569478e34ef1ca1d4fb9a8
Zuzull <113565616+ZulpaAgustin@users.noreply.github.com>
2025-07-18 17:50:11 +0700

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold text-pink-700">Halo Para Mentor JIDA!</h1>
-      <p className="mt-4 text-lg text-gray-600">Ini tugas pertama saya sebagai peserta di Fase 2</p>
-      <p className="text-lg text-gray-600">Mohon Bimbingannya!</p>
-    </main>
-  );
-}
